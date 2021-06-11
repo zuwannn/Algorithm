70faae744c534204d2bd772bf6759c8f1a93af05
zuwannn <suwanneethongtha@gmail.com>
2021-06-11 13:54:50 +0700

    shell sort update

--- ShellSort.py
+++ ShellSort.py
@@ -9,3 +9,14 @@ def shell_sort(inputList):
             j = i
 
             #sort the sublist for this gap
+            while j>= gap and inputList[j-gap] >temp:
+                inputList[j] = inputList[j-gap]
+                j = j-gap
+            inputList[j] = temp
+        
+        #reduce the gap for the next element
+        gap = gap//2
+
+list = [19,2,31,45,6,11,121,27]
+shell_sort(list)
+print(list)
