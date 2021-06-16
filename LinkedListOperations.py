840054dc8642ead07b96aa186ce9e771b393a59c
zuwannn <suwanneethongtha@gmail.com>
2021-06-16 16:45:07 +0700

    Create LinkedListOperations.py

---
+++ LinkedListOperations.py
@@ -0,0 +1,86 @@
+# linked list operation
+
+# create node
+class Node:
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # insert at a beginning
+    def insertAtBeginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # insert after node
+    def insertAfter(self, node, data):
+        if node is None:
+            print("the given previous node must inLinkedList.")
+
+        new_node = Node(data)
+        new_node.next = node.next
+        node.next = new_node
+
+    # insert at the end
+    def insertAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while (last.next):
+            last = last.next
+        last.next = new_node
+
+    # delete node
+    def deleteNode(self, position):
+        if self.head == None:
+            return
+        temp_node = self.head
+        if position == 0:
+            self.head = temp_node.next
+            temp_node = None
+            return
+
+        # find key to delete
+        for i in range(position-1):
+            temp_node = temp_node.next
+            if temp_node is None:
+                break
+
+        # if key is not present
+        if temp_node is None:
+            return
+
+        if temp_node.next is None:
+            return
+
+        next = temp_node.next.next
+        temp_node.next = None
+        temp_node.next = next
+
+    def printList(self):
+        temp_node = self.head
+        while (temp_node):
+            print(str(temp_node.item) + " ", end="")
+            temp_node = temp_node.next
+
+
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.insertAtEnd(1)
+    llist.insertAtBeginning(2)
+    llist.insertAtBeginning(3)
+    llist.insertAtEnd(4)
+    llist.insertAfter(llist.head.next, 5)
+
+    print("Linked List: ")
+    llist.printList()
+
+    print("\nAfter deleting an element: ")
+    llist.deleteNode(3)
+    llist.printList()
