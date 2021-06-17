16c0e98b25cfcbfb361624ab92535f471697f0ab
zuwannn <suwanneethongtha@gmail.com>
2021-06-17 14:58:03 +0700

    Update Dequeue.py

--- Dequeue.py
+++ Dequeue.py
@@ -14,6 +14,15 @@ class Dequeue:
     def addFront(self, item):
         self.items.insert(0, item)
 
+    def removeFront(self):
+        return self.items.pop(0)
+
+    def removeRear(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
 
 if __name__ == "__main__":
     d = Dequeue()
