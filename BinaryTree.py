2dc5ea7db7b1aa6674369ef830f2a4658d693fb9
zuwannn <suwanneethongtha@gmail.com>
2021-06-18 19:44:29 +0700

    Update BinaryTree.py

--- BinaryTree.py
+++ BinaryTree.py
@@ -9,13 +9,29 @@ class Node:
     # root -> left -> right
     def PreOrder(self):
         print(self.value, end=" ")
+        if self.left:
+            self.left.PreOrder()
+        if self.right:
+            self.right.PreOrder()
 
+    # left -> root -> right
     def InOrder(self):
+        if self.left:
+            self.left.InOrder()
+        print(self.value, end=" ")
+        if self.right:
+            self.right.InOrder()
 
+    # left -> right -> root
     def PostOrder(self):
+        if self.left:
+            self.left.PostOrder()
+        if self.right:
+            self.right.PostOrder()
+        print(self.value, end=" ")
 
-        
-if __name__ == "__mai__":
+
+if __name__ == "__main__":
 
     root = Node(1)
     root.left = Node(2)
@@ -23,9 +39,10 @@ if __name__ == "__mai__":
     root.left.left = Node(4)
 
     print("Pre order traversal: ", end="")
-    
+    root.PreOrder()
 
-    print("\In order traversal: ", end="")
-
+    print("\nIn order traversal: ", end="")
+    root.InOrder()
 
     print("\nPost order traversal: ", end="")
+    root.PostOrder()
