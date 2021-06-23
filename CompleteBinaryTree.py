22a8e6ae01503a136f31a2e17916c9128fd80cf0
zuwannn <suwanneethongtha@gmail.com>
2021-06-23 19:54:09 +0700

    Update CompleteBinaryTree.py

--- CompleteBinaryTree.py
+++ CompleteBinaryTree.py
@@ -3,8 +3,8 @@
 complete binary tree ก็เหมือนกับ full binary tree แตกต่างกัน คือ
 1.ทุกlevel ต้องเต็มหมด
 2.leaf node ต้องเอนไปทางขวา
-3.leaf node สุดท้ายอาจไม่มีพี่น้องทางขวา
-(completeBTไม่จำเป็นต้องเป็น fullBT)
+3.leaf node สุดท้าย'อาจ'ไม่มีพี่น้องทางขวาก็ได้
+(completeBT 'ไม่จำเป็นต้องเป็น' fullBT)
 '''
 
 class Node:
@@ -13,12 +13,24 @@ class Node:
         self.left = None
         self.right = None
 
-
 # count the number of node
-def count_node(root):
+def count_nodes(root):
     if root is None:
         return 0
-    return
+    return ( 1 + count_nodes(root.left) + count_nodes(root.right) )
+
+# check if the tree is complete binary tree
+def isComplete(root, index, numberNodes):
+    # check if the tree is empty
+    if root is None:
+        return True
+
+    if index >= numberNodes:
+        return False
+
+    return (isComplete(root.left, 2*index+1, numberNodes) and
+            isComplete(root.right, 2*index+2, numberNodes))
+
 
 if __name__=="__main__":
     root = Node(1)
@@ -26,3 +38,14 @@ if __name__=="__main__":
     root.right = Node(3)
     root.left.left = Node(4)
     root.left.right = Node(5)
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+    root.right.right.left = Node(8)
+    
+    node_count = count_nodes(root)
+    index = 0
+
+    if isComplete(root, index, node_count):
+        print("the tree is a complete binary tree")
+    else:
+        print("the tree is not a complete binary tree")
